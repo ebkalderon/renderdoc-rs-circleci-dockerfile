f3ba0a8567e63145d3d724f951ff1f0ce0ba4c1b
Eyal Kalderon <ebkalderon@gmail.com>
2019-05-19 20:12:45 +0800

    Fix Dockerfile, manually push initial build

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -12,21 +12,22 @@ FROM circleci/rust:${IMAGE_TAG:-latest}
 RUN \\
     echo 'deb http://ftp.debian.org/debian unstable main contrib non-free' | sudo tee /etc/apt/sources.list.d/unstable.list > /dev/null && \\
     sudo apt-get -y update && \\
-    sudo apt-get -y install --no-install-recommends -t unstable renderdoc \\
+    sudo apt-get -y install --no-install-recommends -t unstable renderdoc && \\
     sudo apt-get -y clean
 
 # Install 'bats' for testing Docker container
 RUN \\
-    git clone https://github.com/sstephenson/bats.git && \\
-    cd bats && \\
-    ./install.sh /usr/local && \\
-    cd .. && \\
-    rm -rf bats
+    cd ~ && \\
+    git clone https://github.com/sstephenson/bats.git ~/bats && \\
+    cd ~/bats && \\
+    sudo ./install.sh /usr/local && \\
+    cd - && \\
+    rm -rf ~/bats
 
 # Install dependencies for 'scripts/tap-to-junit.sh'
 RUN \\
-    perl -MCPAN -e 'install TAP::Parser' && \\
-    perl -MCPAN -e 'install XML::Generator'
+    sudo perl -MCPAN -e 'install TAP::Parser' && \\
+    sudo perl -MCPAN -e 'install XML::Generator'
 
 ENV DISPLAY :99
 
