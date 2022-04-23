bfb4366d8dc4ec025dc0b0788b551bb08b6e5b64
Eyal Kalderon <ebkalderon@gmail.com>
2022-04-23 17:06:12 -0400

    Fetch from https://deb.debian.org/debian

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -10,7 +10,9 @@ FROM cimg/rust:${IMAGE_TAG:-latest}
 
 # Install 'renderdoc' on top of base image
 RUN \\
-    echo 'deb http://ftp.debian.org/debian unstable main contrib non-free' | sudo tee /etc/apt/sources.list.d/unstable.list > /dev/null && \\
+    echo 'deb http://deb.debian.org/debian unstable main contrib non-free' | sudo tee /etc/apt/sources.list.d/unstable.list > /dev/null && \\
+    APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 648ACFD622F3D138 && \\
+    APT_KEY_DONT_WARN_ON_DANGEROUS_USAGE=1 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 0E98404D386FA1D9 && \\
     sudo apt-get -y update && \\
     sudo apt-get -y install --no-install-recommends -t unstable renderdoc && \\
     sudo apt-get -y clean
